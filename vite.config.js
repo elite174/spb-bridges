0805a67b08aead835f49e4b366c0d63af7cc5f10
Vladislav <lipatov.xex@gmail.com>
2022-09-11 19:53:12 +0300

    Small folder fix

--- vite.config.js
+++ vite.config.js
@@ -21,17 +21,17 @@ export default defineConfig({
         theme_color: "#222",
         icons: [
           {
-            src: "public/bridge-192x192.png",
+            src: "assets/bridge-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "public/bridge-512x512.png",
+            src: "assets/bridge-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "public/bridge-512x512.png",
+            src: "assets/bridge-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
